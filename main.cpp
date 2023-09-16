1970335870c6ab171674f3c56739cca9704131e5
Mark Jarzynski <markj1@umbc.edu>
2023-09-16 15:54:56 -0400

    Added Dear ImGui. Print EASTL, GLFW, Dear ImGUI, and Vulkan versions"

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <EASTL/vector.h>
+#include <EASTL/version.h>
 #include <stdio.h>
 
 #include <vulkan/vulkan.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+
+#include <imgui.h>
 
 // These new operators are required by EASTL
 void* __cdecl operator new[](size_t size, const char* pName, int flags, unsigned debugFlags, const char* file, int line)
@@ -16,6 +19,7 @@ void* __cdecl operator new[](size_t size, size_t alignment, size_t alignmentOffs
     return new uint8_t[size];
 }
 
+
 int main( int argc, char** argv)
 {
     eastl::vector<uint32_t> v = {0, 1, 2};
@@ -26,6 +30,14 @@ int main( int argc, char** argv)
     }
 
     glfwInit();
+
+    printf("EASTL version: %s\n", EASTL_VERSION);
+    printf("GLFW version: %s\n", glfwGetVersionString());
+    printf("Dear ImGui version: %s\n", ImGui::GetVersion());
+
+    uint32_t instanceVersion = VK_API_VERSION_1_0;
+    vkEnumerateInstanceVersion(&instanceVersion);
+    printf("Vulkan version: %d.%d.%d\n", VK_VERSION_MAJOR(instanceVersion), VK_VERSION_MINOR(instanceVersion), VK_VERSION_PATCH(instanceVersion));
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World!", NULL, NULL);
@@ -43,10 +55,15 @@ int main( int argc, char** argv)
 
     glfwMakeContextCurrent(window);
 
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
     while (!glfwWindowShouldClose(window))
     {
         glfwSwapBuffers(window);
         glfwPollEvents();
+
+        ImGui::ShowDemoWindow();
     }
 
     glfwDestroyWindow(window);
