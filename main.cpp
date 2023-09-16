bd3acc97836bdf46aa2b5e05e18e6968c07183f4
Mark Jarzynski <markj1@umbc.edu>
2023-09-16 14:49:44 -0400

    Added EASTL test and GLFW test in main(). Changed line endings to LF

--- main.cpp
+++ main.cpp
@@ -1,6 +1,55 @@
+#include <EASTL/vector.h>
+#include <stdio.h>
+
 #include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+// These new operators are required by EASTL
+void* __cdecl operator new[](size_t size, const char* pName, int flags, unsigned debugFlags, const char* file, int line)
+{
+    return new uint8_t[size];
+}
+
+void* __cdecl operator new[](size_t size, size_t alignment, size_t alignmentOffset, const char* pName, int flags, unsigned debugFlags, const char* file, int line)
+{
+    return new uint8_t[size];
+}
 
 int main( int argc, char** argv)
 {
+    eastl::vector<uint32_t> v = {0, 1, 2};
+
+    for (uint32_t i : v)
+    {
+        printf("%d\n", i);
+    }
+
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World!", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    if (glfwVulkanSupported())
+    {
+        printf("GLFW: Vulkan supported\n");
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
