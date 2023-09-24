e4888fa122d5d9cb396d2ba591c7f00225bc9dbc
Mark Jarzynski <markj1@umbc.edu>
2023-09-24 01:13:04 -0400

    Re-added EASTL as a submodule, abonded using cmake ExternalProject. Noted a cyclical submodule dependency with EASTL in the README. Updated cmake to use add_subdirectory to include EASTL and it's subcomponents, which seems to work better than anything else I tried.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Raptor Engine
 
-My implementation of the Raptor Engine from the book Mastering Graphics Programming with Vulkan
+This is my implementation of the Raptor Engine from the book Mastering Graphics Programming with Vulkan.
 
+## Notes on EASTL
+
+Warning, EASTL has cyclical submodule dependencies!!! Do not run git submodule update --recursive. Manually, cd into Common/External/EASTL and do a single git submodule {init, update}.
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 #include <EASTL/vector.h>
 #include <EASTL/version.h>
-//#include <EAStdC/EASprintf.h>
+#include <EAStdC/EASprintf.h>
 #include <stdio.h>
 
 #include <vulkan/vulkan.h>
@@ -25,21 +25,21 @@ void* __cdecl operator new[](size_t size, size_t alignment, size_t alignmentOffs
 
 void debug_print_versions()
 {
-    printf("Raptor version: %s\n", RAPTOR_VERSION);
-    printf("EASTL version: %s\n", EASTL_VERSION);
-    printf("GLFW version: %s\n", glfwGetVersionString());
-    printf("Dear ImGui version: %s\n", ImGui::GetVersion());
+    EA::StdC::Printf("Raptor version: %s\n", RAPTOR_VERSION);
+    EA::StdC::Printf("EASTL version: %s\n", EASTL_VERSION);
+    EA::StdC::Printf("GLFW version: %s\n", glfwGetVersionString());
+    EA::StdC::Printf("Dear ImGui version: %s\n", ImGui::GetVersion());
 
     uint32_t instanceVersion = VK_API_VERSION_1_0;
     vkEnumerateInstanceVersion(&instanceVersion);
-    printf("Vulkan version: %d.%d.%d\n", VK_VERSION_MAJOR(instanceVersion), VK_VERSION_MINOR(instanceVersion), VK_VERSION_PATCH(instanceVersion));
+    EA::StdC::Printf("Vulkan version: %d.%d.%d\n", VK_VERSION_MAJOR(instanceVersion), VK_VERSION_MINOR(instanceVersion), VK_VERSION_PATCH(instanceVersion));
 }
 
 int main( int argc, char** argv)
 {
     debug_print_versions();
 
-    Raptor::Graphics::Window window {640, 480, "Raptor"};
+    Raptor::Graphics::Window window {1920, 1080, "Raptor"};
 
     //glfwMakeContextCurrent(window);
 
