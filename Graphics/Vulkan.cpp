6588e879e405b4c95d88827ffc413fdf969dc1a3
Mark Jarzynski <markj1@umbc.edu>
2023-10-01 23:31:03 -0400

    Finished CreateInstance() and started CreateDevices() in Vulkan.cpp

--- Graphics/Vulkan.cpp
+++ Graphics/Vulkan.cpp
@@ -1,28 +1,44 @@
 #include "Vulkan.h"
 #include "Config.h"
 #include "Debug.h"
-#include "Type.h"
+//#include "Type.h"
+#include "Defines.h"
 #include <EAStdC/EASprintf.h>
+#include <EASTL/fixed_vector.h>
 
 namespace Raptor
 {
 namespace Graphics
 {
 
+static const char* s_requested_layers[] = {
+    "VK_LAYER_KHRONOS_validation",
+};
+
+static const char* s_requested_extensions[] = {
+    VK_KHR_SURFACE_EXTENSION_NAME,
+#ifdef VK_USE_PLATFORM_WIN32_KHR
+    VK_KHR_WIN32_SURFACE_EXTENSION_NAME,
+#endif
+};
+
 Vulkan::Vulkan(Window& window)
     : window(&window)
 {
-    createInstance();
+    CreateInstance();
+    CreatePhysicalDevices();
 }
 
 Vulkan::~Vulkan()
 {
-    destroyInstance();
+    DestroyPhysicalDevices();
+    DestroyInstance();
 }
 
-void Vulkan::createInstance()
+void Vulkan::CreateInstance()
 {
     VkResult result;
+    allocationCallbacks = nullptr;
 
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -35,18 +51,189 @@ void Vulkan::createInstance()
     VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     createInfo.pApplicationInfo = &appInfo;
+    createInfo.enabledLayerCount = ARRAY_SIZE(s_requested_layers);
+    createInfo.ppEnabledLayerNames = s_requested_layers;
+    createInfo.enabledExtensionCount = ARRAY_SIZE(s_requested_extensions);
+    createInfo.ppEnabledExtensionNames = s_requested_extensions;
 
-    result = vkCreateInstance(&createInfo, nullptr, &instance);
+#ifdef VULKAN_DEBUG
+    VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
+    debugCreateInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    debugCreateInfo.pfnUserCallback = DebugUtilsCallback;
+    debugCreateInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT;
+    debugCreateInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT;
+
+    createInfo.pNext = &debugCreateInfo;
+#endif
+
+    result = vkCreateInstance(&createInfo, allocationCallbacks, &instance);
     ASSERT_MESSAGE(result == VK_SUCCESS, "[Vulkan] Error: Failed to create instance. code(%u).", result);
 
+#ifdef VULKAN_DEBUG
     uint32 numExtensions;
+    vkEnumerateInstanceExtensionProperties( nullptr, &numExtensions, nullptr );
+    eastl::vector<VkExtensionProperties> extensions(numExtensions);
+    //extensions.reserve(numExtensions);
+    vkEnumerateInstanceExtensionProperties(nullptr, &numExtensions, extensions.data());
+    for (VkExtensionProperties ext : extensions)
+    {
+        if (strcmp(ext.extensionName, VK_EXT_DEBUG_UTILS_EXTENSION_NAME) == 0)
+        {
+            flags |= DebugUtilsExtensionExist;
+            break;
+        }
+    }
 
+    if (flags & Flags::DebugUtilsExtensionExist)
+    {
+        PFN_vkCreateDebugUtilsMessengerEXT vkCreateDebugUtilsMessengerEXT = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+        if (vkCreateDebugUtilsMessengerEXT != nullptr)
+            vkCreateDebugUtilsMessengerEXT(instance, &debugCreateInfo, allocationCallbacks, &debugUtilsMessenger);
+        else
+            Raptor::Debug::Log("[Vulkan] Warning: Failed to setup debug messenger.");
+    }
+    else
+    {
+        Raptor::Debug::Log("[Vulkan] Warning: Extension %s for debugging does not exist!", VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    }
+#endif 
 }
 
-void Vulkan::destroyInstance()
+void Vulkan::DestroyInstance()
 {
-    vkDestroyInstance(instance, nullptr);
+#ifdef VULKAN_DEBUG
+    if (flags & Flags::DebugUtilsExtensionExist)
+    {
+        PFN_vkDestroyDebugUtilsMessengerEXT vkDestroyDebugUtilsMessengerEXT = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+        if (vkDestroyDebugUtilsMessengerEXT != nullptr)
+            vkDestroyDebugUtilsMessengerEXT(instance, debugUtilsMessenger, allocationCallbacks);
+    }
+#endif
+
+    vkDestroyInstance(instance, allocationCallbacks);
 }
+
+void Vulkan::CreatePhysicalDevices()
+{
+    VkResult result;
+    uint32 numPhysicalDevices;
+     
+    result = vkEnumeratePhysicalDevices(instance, &numPhysicalDevices, NULL);
+    ASSERT_MESSAGE(result == VK_SUCCESS, "[Vulkan] Error: Failed to enumerate physical devices. code(%u).", result);
+
+    eastl::vector<VkPhysicalDevice> physicalDevices(numPhysicalDevices);   
+
+    result = vkEnumeratePhysicalDevices(instance, &numPhysicalDevices, physicalDevices.data());
+    ASSERT_MESSAGE(result == VK_SUCCESS, "[Vulkan] Error: Failed to enumerate physical devices. code(%u).", result);
+
+    VkPhysicalDevice discrete = VK_NULL_HANDLE;
+    VkPhysicalDevice integrated = VK_NULL_HANDLE;
+    for (VkPhysicalDevice pDevice : physicalDevices)
+    {
+        vkGetPhysicalDeviceProperties(pDevice, &physicalDeviceProperties);
+
+        if (physicalDeviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+        {
+            if (GetFamilyQueue(pDevice))
+            {
+                discrete = pDevice;
+                break;
+            }
+        }
+        else if (physicalDeviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU)
+        {
+            if (GetFamilyQueue(pDevice))
+            {
+                integrated = pDevice;
+            }
+        }
+    }
+
+    if (discrete != VK_NULL_HANDLE)
+    {
+        physicalDevice = discrete;
+    }
+    else if (integrated != VK_NULL_HANDLE)
+    {
+        physicalDevice = integrated;
+    }
+    else
+    {
+        ASSERT_MESSAGE(false, "[Vulkan] Error: Failed to find a suitable GPU device.");
+    }
+
+    uint32 deviceExtensionCount = 0;
+    vkEnumerateDeviceExtensionProperties(physicalDevice, nullptr, &deviceExtensionCount, nullptr);
+    eastl::vector<VkExtensionProperties> extensions(deviceExtensionCount);
+    vkEnumerateDeviceExtensionProperties(physicalDevice, nullptr, &deviceExtensionCount, extensions.data());
+
+    for (VkExtensionProperties extension : extensions)
+    {
+        if (strcmp(extension.extensionName, VK_KHR_DYNAMIC_RENDERING_EXTENSION_NAME) == 0)
+        {
+            flags |= Flags::DynamicRenderingExtensionExists;
+            break;
+        }
+    }
+
+    vkGetPhysicalDeviceProperties(physicalDevice, &physicalDeviceProperties);
+
+    Raptor::Debug::Log("[Vulkan] Info: GPU Used: %s\n", physicalDeviceProperties.deviceName);
+}
+
+void Vulkan::DestroyPhysicalDevices()
+{
+
+}
+
+VkBool32 Vulkan::GetFamilyQueue(VkPhysicalDevice pDevice)
+{
+    uint32 familyQueueCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(pDevice, &familyQueueCount, nullptr);
+
+    eastl::vector<VkQueueFamilyProperties> queueFamilies(familyQueueCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(pDevice, &familyQueueCount, queueFamilies.data());
+
+    VkBool32 surfaceSupported = VK_FALSE;
+
+    for (uint32 i = 0; i < queueFamilies.size(); i++)
+    {
+        if (queueFamilies[i].queueCount > 0 && queueFamilies[i].queueFlags & (VK_QUEUE_GRAPHICS_BIT | VK_QUEUE_COMPUTE_BIT))
+        {
+            vkGetPhysicalDeviceSurfaceSupportKHR(pDevice, i, surface, &surfaceSupported);
+
+            if (surfaceSupported)
+            {
+                mainQueueFamilyIndex = i;
+                break;
+            }
+        }
+    }
+
+    return surfaceSupported;
+}
+
+void Vulkan::CreateSurface()
+{
+    VkResult result = glfwCreateWindowSurface(instance, window->GetGLFWwindow(), allocationCallbacks, &surface);
+    ASSERT_MESSAGE(result == VK_SUCCESS, "[Vulkan] Error: Failed to create window surface.");
+}
+
+void Vulkan::DestroySurface()
+{
+    vkDestroySurfaceKHR(instance, surface, allocationCallbacks);
+}
+
+#ifdef VULKAN_DEBUG
+static VkBool32 DebugUtilsCallback(VkDebugUtilsMessageSeverityFlagBitsEXT severity, VkDebugUtilsMessageTypeFlagsEXT types, const VkDebugUtilsMessengerCallbackDataEXT* callback_data, void* user_data)
+{
+    ASSERT_MESSAGE(!(severity & (VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT)),
+        "Message ID: %s %i\nMessage: %s\n", 
+        callback_data->pMessageIdName, callback_data->messageIdNumber, callback_data->pMessage);
+
+    return VK_FALSE;
+}
+#endif
 
 } // namespace Graphics
 } // namespace Raptor
--- Graphics/Vulkan.h
+++ Graphics/Vulkan.h
@@ -1,7 +1,15 @@
 #pragma once
 
+#if (_MSC_VER)
+#define VK_USE_PLATFORM_WIN32_KHR
+#endif
 #include <vulkan/vulkan.h>
 #include "Window.h"
+#include "Debug.h"
+#include "Log.h"
+#include "Type.h"
+
+#define VULKAN_DEBUG
 
 namespace Raptor
 {
@@ -24,14 +32,41 @@ public:
 private:
 
     // Vulkan Instance
-    void createInstance();
-    void destroyInstance();
+    void CreateInstance();
+    void DestroyInstance();
+
+    void CreatePhysicalDevices();
+    void DestroyPhysicalDevices();
+
+    void CreateSurface();
+    void DestroySurface();
+
+    VkBool32 GetFamilyQueue(VkPhysicalDevice pDevice);
+
+private:
     
     VkInstance instance;
+    VkAllocationCallbacks* allocationCallbacks;
+    VkDebugUtilsMessengerEXT debugUtilsMessenger;
+    VkPhysicalDevice physicalDevice;
+    VkPhysicalDeviceProperties physicalDeviceProperties;
+    VkSurfaceKHR surface;
+    uint32 mainQueueFamilyIndex;
+
+    enum Flags
+    {
+        DebugUtilsExtensionExist        = 0x1 << 0,
+        DynamicRenderingExtensionExists = 0x1 << 1,
+    };
+    uint32 flags = 0u;
 
 }; // class Vulkan
 
 static bool check_result(VkResult result);
+
+#ifdef VULKAN_DEBUG
+static VkBool32 DebugUtilsCallback(VkDebugUtilsMessageSeverityFlagBitsEXT severity, VkDebugUtilsMessageTypeFlagsEXT types, const VkDebugUtilsMessengerCallbackDataEXT* callback_data, void* user_data);
+#endif
 
 } // namespace Graphics
 } // namespace Raptor
