b6228c7e9b2216ac56de32855a188984da4ecb72
Mark Jarzynski <mark.jarzynski@gmail.com>
2023-10-15 21:46:19 -0400

    macos debug break.

--- Debug/Debug.h
+++ Debug/Debug.h
@@ -7,7 +7,11 @@ namespace Raptor
 namespace Debug
 {
 
+#if defined(_MSC_VER)
 #define DEBUG_BREAK __debugbreak();
+#elif defined(__aarch64__) && defined(__APPLE__)
+#define DEBUG_BREAK __builtin_debugtrap();
+#endif
 
 #define ASSERT(condition) if (!(condition)) { DEBUG_BREAK }
 #define ASSERT_MESSAGE(condition, message, ...) if (!(condition)) { Raptor::Debug::Log(message, __VA_ARGS__); DEBUG_BREAK }
